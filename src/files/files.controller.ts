bee3c63bd58f9127bc5b111fb35ae74b6ef0e694
IvanDev <ivanperezrebollo@hotmail.com>
2025-10-20 14:10:36 +0200

    fin seccion 12

---
+++ src/files/files.controller.ts
@@ -0,0 +1,53 @@
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Res,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { FilesService } from './files.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter, fileNamer } from './helpers';
+import { diskStorage } from 'multer';
+import { ConfigService } from '@nestjs/config';
+
+@Controller('files')
+export class FilesController {
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService,
+  ) {}
+  @Get('product/:imageName')
+  findProductImage(@Res() res, @Param('imageName') imageName: string) {
+    const path = this.filesService.getStaticProductImage(imageName);
+    res.sendFile(path);
+  }
+
+  @Post('product')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter,
+      // limits: { fileSize: 1024 * 1024 }, // 1MB
+      storage: diskStorage({
+        destination: './static/products',
+        filename: fileNamer,
+      }),
+    }),
+  )
+  uploadProductImage(@UploadedFile() file: Express.Multer.File) {
+    if (!file) {
+      throw new BadRequestException('Make sure that the file is an image');
+    }
+    console.log(file);
+    // Si fuese un proyecto en nube, mejor usar servicios como AWS S3, Cloudinary, etc.
+    // En local no importa que guardemos las imagenes en el servidor
+    const secureUrl = `${this.configService.get('HOST_API')}/files/product/${
+      file.filename
+    }`;
+    return { secureUrl };
+  }
+}
